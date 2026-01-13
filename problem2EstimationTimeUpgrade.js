d807e2478fdf9cfa6205294eaaf03b7f39ffa279
soumitha18 <soumithaalpineti@gmail.com>
2026-01-13 10:46:57 +0530

    fix: calculate package delivery cost and discounts and initialize vehicle availability tracking

---
+++ problem2EstimationTimeUpgrade.js
@@ -0,0 +1,73 @@
+let input = `100 5
+PKG1 50 30 OFR001
+PKG2 75 125 OFFR0008
+PKG3 175 100 OFR003
+PKG4 110 60 OFR002
+PKG5 155 95 NA
+2 70 200`;
+
+const packages= input.trim().split("\n");
+
+const [base_delivery_cost , no_of_packages]= packages[0].split(" ").map(Number)
+const [no_of_vehicles, max_speed, max_weight] = packages[packages.length-1].trim().split(" ").map(Number);
+
+const offers= {
+    OFR001: {
+        discount : 10,
+        min_distance: 0,
+        max_distance: 199,
+        min_weight: 70,
+        max_weight: 200
+    },
+    OFR002: {
+        discount: 7,
+        min_distance: 50,
+        max_distance: 150,
+        min_weight: 100,
+        max_weight: 250
+    },
+    OFR003: {
+        discount: 5,
+        min_distance: 50,
+        max_distance: 250,
+        min_weight: 10,
+        max_weight: 150
+    }
+}
+
+const packages_delivery_info = []
+
+for(let i=1; i <= no_of_packages; i++){
+    if (!packages[i]) continue;
+
+    const package = packages[i].split(" ")
+    const pkg_id = package[0]
+    const pkg_weight = Number(package[1])
+    const distance = Number(package[2])
+    const offer_code = package[3].toUpperCase()
+
+    if(pkg_weight < 0 || distance < 0){
+        console.log(`invalid package details for ${pkg_id}!`)
+        continue;
+    }
+     
+    let cost = base_delivery_cost + ( pkg_weight * 10 ) + ( distance * 5 )
+    // console.log(cost, "cost:::")
+
+    let discount = 0
+    if(offers[offer_code]){
+        const offer = offers[offer_code]
+        if(distance >= offer.min_distance && distance <= offer.max_distance && pkg_weight >= offer.min_weight && pkg_weight <= offer.max_weight){
+            discount = Math.floor(cost * (offer.discount / 100))
+        }
+    }
+    // console.log(discount, "discount:::")
+
+    const total_cost = cost - discount
+
+    packages_delivery_info.push({pkg_id, pkg_weight, distance, cost, discount, total_cost, delivery_time: 0, delivered: false})
+    // console.log(pkg_id, discount, total_cost)
+}
+
+let vehicles = Array(no_of_vehicles).fill(0);
+let delivered_count = 0;
