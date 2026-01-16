bdd84bb89d05da380219652ece12ed87624165ba
soumitha18 <soumithaalpineti@gmail.com>
2026-01-16 13:04:27 +0530

    added choice to choose to play

--- src/index.js
+++ src/index.js
@@ -1,5 +1,30 @@
-function main() {
-    console.log(`K I K I   E X P R E S S`);
+import inquirer from "inquirer";
+
+async function main() {
+  console.log(`
+    
+                  K I K I   E X P R E S S
+                  
+`);
+     
+  while (true) {
+    const { choice } = await inquirer.prompt({
+      type: "list",
+      name: "choice",
+      message: "Select an option",
+      choices: ["Calculate delivery cost", "Calculate delivery time", "Exit"]
+    });
+
+    if (choice === "Exit") break;
+
+    if (choice === "Calculate delivery cost") {
+      console.log(choice)
+    }
+
+    if (choice === "Calculate delivery time") {
+      console.log("Delivery time calculation coming next......");
+    }
+  }
 }
 
 main();
