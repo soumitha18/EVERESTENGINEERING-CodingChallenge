18d3e85f6abe547afb4a8d530a2e382fcc38ad91
soumitha18 <soumithaalpineti@gmail.com>
2026-01-16 12:22:36 +0530

    add offers

---
+++ src/config/offers.js
@@ -0,0 +1,7 @@
+const offers = {
+  OFR001: { discount: 10, minDistance: 0, maxDistance: 199, minWeight: 70, maxWeight: 200 },
+  OFR002: { discount: 7, minDistance: 50, maxDistance: 150, minWeight: 100, maxWeight: 250 },
+  OFR003: { discount: 5, minDistance: 50, maxDistance: 250, minWeight: 10, maxWeight: 150 }
+};
+
+export default offers;
